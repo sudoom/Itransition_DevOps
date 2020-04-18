5877becf43530659b217e7d003c3d7e3e017fbc7
Vadzim Dziadziulia <32463123+sudoom@users.noreply.github.com>
2020-04-18 16:07:47 +0300

    Delete nginx.sh

--- Docker/Docker_run/nginx.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-docker pull nginx
-mkdir -p `pwd`/nginx/etc
-docker run --name docker-nginx -v `pwd`/nginx/etc:/etc/nginx -p 8080:80 -d nginx
-sleep 10
-curl -l localhost:8080
-docker stop docker-nginx
