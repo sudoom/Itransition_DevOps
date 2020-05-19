8bc173d3af2e5f600716ea5cd1787960fa3972b7
Vadzim <vadi.vad.dvp@gmail.com>
2020-05-19 23:26:33 +0300

    refactor instance_id

--- Task_7_Cloudformation_Terraform/terraform/modules/EC2/outputs.tf
+++ Task_7_Cloudformation_Terraform/terraform/modules/EC2/outputs.tf
@@ -1,7 +1,6 @@
 # Output the IDs of the EC2 instances created
 output "instance_ids" {
-  value = [
-    aws_instance.instance.*.id]
+  value = aws_instance.instance.*.id
 }
 
 output "availability_zone" {
